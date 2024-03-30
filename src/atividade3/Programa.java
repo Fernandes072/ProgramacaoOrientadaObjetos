75591f490db33c6ad2bbef1e46524c5f1a9422dc
Fernandes072 <joaofilho8274@gmail.com>
2024-03-30 19:13:52 -0300

    Atividade 3 - parte 3

--- src/atividade3/Programa.java
+++ src/atividade3/Programa.java
@@ -10,6 +10,8 @@ public class Programa {
 
 		Scanner s = new Scanner(System.in);
 		List<Cliente> clientes = new ArrayList<Cliente>();
+		List<Cliente> gerentes = new ArrayList<Cliente>();
+		gerentes.add(new Cliente(0, "0", "0", "0"));
 
 		menu();
 		String opcao = s.next();
@@ -18,7 +20,7 @@ public class Programa {
 				if (opcao.equals("1")) {
 					cadastrarCliente(s, clientes);
 				} else if (opcao.equals("2")) {
-					efetuarLogin(s, clientes);
+					efetuarLogin(s, clientes, gerentes);
 				} else {
 					throw new BancoException("Erro: Opção inválida!");
 				}
@@ -34,12 +36,23 @@ public class Programa {
 
 	}
 
-	private static void efetuarLogin(Scanner s, List<Cliente> clientes) {
+	private static void efetuarLogin(Scanner s, List<Cliente> clientes, List<Cliente> gerentes) {
 		Cliente reserva = null;
 		System.out.print("Código: ");
 		int codigo = s.nextInt();
 		System.out.print("Senha: ");
 		String senha = s.next();
+
+		boolean eGerente = false;
+
+		for (Cliente gerente : gerentes) {
+			if (codigo == gerente.getCodigo()) {
+				gerente.login(senha);
+				reserva = gerente;
+				eGerente = true;
+				break;
+			}
+		}
 
 		for (Cliente cliente : clientes) {
 			if (codigo == cliente.getCodigo()) {
@@ -53,35 +66,142 @@ public class Programa {
 			throw new BancoException("Erro: Código inválido!");
 		}
 
+		if (eGerente) {
+			opcoesGerente(s, clientes);
+		} else {
+			opcoesCliente(s, reserva, clientes);
+		}
+	}
+
+	private static void opcoesCliente(Scanner s, Cliente cliente, List<Cliente> clientes) {
 		System.out.println();
-		menuLogado();
+		menuCliente();
 		String opcao = s.next();
 		while (!opcao.equals("9")) {
 			try {
 				if (opcao.equals("1")) {
-					criarConta(s, reserva, clientes);
+					criarConta(s, cliente, clientes);
 				} else if (opcao.equals("2")) {
-					deposito(s, reserva);
+					deposito(s, cliente);
 				} else if (opcao.equals("3")) {
-					saque(s, reserva);
+					saque(s, cliente);
 				} else if (opcao.equals("4")) {
-					transferencia(s, reserva, clientes);
+					transferencia(s, cliente, clientes);
 				} else if (opcao.equals("5")) {
-					exibirContas(reserva);
+					exibirContas(cliente);
 				} else if (opcao.equals("6")) {
-					exibirExtrato(s, reserva);
+					exibirExtrato(s, cliente);
 				} else if (opcao.equals("7")) {
-					solicitarAumento(s, reserva);
-				}  else {
+					solicitarAumento(s, cliente);
+				} else {
 					throw new BancoException("Erro: Opção inválida!");
 				}
 			} catch (BancoException e) {
 				System.out.println(e.getMessage());
 			} finally {
 				System.out.println();
-				menuLogado();
+				menuCliente();
 				opcao = s.next();
 			}
+		}
+
+	}
+
+	private static void opcoesGerente(Scanner s, List<Cliente> clientes) {
+		System.out.println();
+		menuGerente();
+		String opcao = s.next();
+		while (!opcao.equals("9")) {
+			try {
+				if (opcao.equals("1")) {
+					listarClientes(s, clientes);
+				} else if (opcao.equals("2")) {
+					listarPedidosAumento(s, clientes);
+				} else if (opcao.equals("3")) {
+					listarClientesBloqueados(s, clientes);
+				} else if (opcao.equals("4")) {
+					concederAumento(s, clientes);
+				} else if (opcao.equals("5")) {
+					desbloquearConta(s, clientes);
+				} else {
+					throw new BancoException("Erro: Opção inválida!");
+				}
+			} catch (BancoException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				System.out.println();
+				menuGerente();
+				opcao = s.next();
+			}
+		}
+
+	}
+
+	private static void desbloquearConta(Scanner s, List<Cliente> clientes) {
+		System.out.print("Código: ");
+		int codigo = s.nextInt();
+
+		Cliente cliente = null;
+		for (Cliente c : clientes) {
+			if (codigo == c.getCodigo() && c.getTentativas() == 0) {
+				cliente = c;
+			}
+		}
+		if (cliente == null) {
+			throw new BancoException("Erro: Código inválido!");
+		}
+		
+		cliente.setTentativas(3);
+		System.out.println("Desbloqueio realizado!");
+	}
+
+	private static void concederAumento(Scanner s, List<Cliente> clientes) {
+		System.out.print("Número da conta: ");
+		int numero = s.nextInt();
+
+		Conta conta = null;
+		for (Cliente cliente : clientes) {
+			for (Conta c : cliente.getContas()) {
+				if (numero == c.getNumero() && c instanceof ContaCorrente) {
+					if (((ContaCorrente) c).getAumentoChequeEspecial() > 0) {
+						conta = c;
+					}
+				}
+			}
+		}
+		if (conta == null) {
+			throw new BancoException("Erro: Número de conta inválido!");
+		}
+		
+		ContaCorrente contaC = (ContaCorrente) conta;
+		contaC.setChequeEspecial(contaC.getChequeEspecial() + contaC.getAumentoChequeEspecial());
+		contaC.setAumentoChequeEspecial(0);
+		System.out.println("Aumento realizado!");
+	}
+
+	private static void listarClientesBloqueados(Scanner s, List<Cliente> clientes) {
+		for (Cliente cliente : clientes) {
+			if (cliente.getTentativas() == 0) {
+				System.out.println(cliente);
+			}
+		}
+	}
+
+	private static void listarPedidosAumento(Scanner s, List<Cliente> clientes) {
+		for (Cliente cliente : clientes) {
+			for (Conta conta : cliente.getContas()) {
+				if (conta instanceof ContaCorrente) {
+					if (((ContaCorrente) conta).getAumentoChequeEspecial() > 0) {
+						System.out.println(conta);
+					}
+				}
+			}
+		}
+	}
+
+	private static void listarClientes(Scanner s, List<Cliente> clientes) {
+		for (Cliente cliente : clientes) {
+			System.out.println(cliente);
 		}
 	}
 
@@ -98,11 +218,11 @@ public class Programa {
 		if (conta == null || conta instanceof ContaPoupanca) {
 			throw new BancoException("Erro: Número de conta inválido!");
 		}
-		
+
 		System.out.print("Valor: ");
 		double valor = s.nextDouble();
 		((ContaCorrente) conta).setAumentoChequeEspecial(valor);
-		System.out.println("Pedido realizado!"); 
+		System.out.println("Pedido realizado!");
 	}
 
 	private static void exibirExtrato(Scanner s, Cliente cliente) {
@@ -118,7 +238,7 @@ public class Programa {
 		if (conta == null) {
 			throw new BancoException("Erro: Número de conta inválido!");
 		}
-		
+
 		for (Transacao transacao : conta.getTransacoes()) {
 			System.out.println(transacao);
 		}
@@ -143,7 +263,7 @@ public class Programa {
 		if (conta == null) {
 			throw new BancoException("Erro: Número de conta inválido!");
 		}
-		
+
 		System.out.print("Número da conta destino: ");
 		int numeroDestino = s.nextInt();
 
@@ -156,11 +276,11 @@ public class Programa {
 		if (contaDestino == null) {
 			throw new BancoException("Erro: Número de conta destino inválido!");
 		}
-		
+
 		System.out.print("Valor: ");
 		double valor = s.nextDouble();
 		conta.transferencia(valor, contaDestino);
-		System.out.println("Transferência realizada!"); 
+		System.out.println("Transferência realizada!");
 	}
 
 	private static void saque(Scanner s, Cliente cliente) {
@@ -176,11 +296,11 @@ public class Programa {
 		if (conta == null) {
 			throw new BancoException("Erro: Número de conta inválido!");
 		}
-		
+
 		System.out.print("Valor: ");
 		double valor = s.nextDouble();
 		conta.saque(valor);
-		System.out.println("Saque realizado!"); 
+		System.out.println("Saque realizado!");
 	}
 
 	private static void deposito(Scanner s, Cliente cliente) {
@@ -196,11 +316,11 @@ public class Programa {
 		if (conta == null) {
 			throw new BancoException("Erro: Número de conta inválido!");
 		}
-		
+
 		System.out.print("Valor: ");
 		double valor = s.nextDouble();
 		conta.deposito(valor);
-		System.out.println("Depósito realizado!"); 
+		System.out.println("Depósito realizado!");
 	}
 
 	private static void criarConta(Scanner s, Cliente cliente, List<Cliente> clientes) {
@@ -270,7 +390,7 @@ public class Programa {
 
 	}
 
-	private static void menuLogado() {
+	private static void menuCliente() {
 		System.out.println("1 - Criar conta");
 		System.out.println("2 - Depósito");
 		System.out.println("3 - Saque");
@@ -278,6 +398,16 @@ public class Programa {
 		System.out.println("5 - Exibir contas");
 		System.out.println("6 - Exibir extrato");
 		System.out.println("7 - Solicitar aumento do cheque especial");
+		System.out.println("9 - Logout");
+		System.out.print("Digite a opção: ");
+	}
+
+	private static void menuGerente() {
+		System.out.println("1 - Listar clientes");
+		System.out.println("2 - Listar pedidos de aumento");
+		System.out.println("3 - Listar contas bloqueadas");
+		System.out.println("4 - Aceitar pedido de aumento");
+		System.out.println("5 - Desbloquear conta");
 		System.out.println("9 - Logout");
 		System.out.print("Digite a opção: ");
 	}
