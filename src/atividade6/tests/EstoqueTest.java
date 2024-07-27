eafe2d548235d205dd517efb22e702003f0c78e0
Fernandes072 <joaofilho8274@gmail.com>
2024-07-27 20:41:43 -0300

    Atividade 6 - parte 4

--- src/atividade6/tests/EstoqueTest.java
+++ src/atividade6/tests/EstoqueTest.java
@@ -4,122 +4,93 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import atividade6.classes.Estoque;
 
 class EstoqueTest {
 
-//	private Estoque estoque;
-//	
-//	@BeforeEach
-//	void criarEstoque() {
-//		estoque = new Estoque();
-//		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-//		estoque.cadastrarProduto("macarrão", "parafuso", 3, 15);
-//		System.out.println(estoque.listarProdutos());
-//	}
-//	
-//	@AfterEach
-//	void apagarEstoque() {
-//		estoque = null;
-//	}
+	private static Estoque estoque;
+	
+	@BeforeAll
+	static void criarEstoque() {
+		estoque = new Estoque();
+		estoque.cadastrarProduto("arroz", "branco", 5, 10);
+		estoque.cadastrarProduto("macarrão", "parafuso", 3, 15);
+	}
+	
 
 	@Test
 	void adicionarDoisProdutosDeveRetornarTamanhoDois() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-		estoque.cadastrarProduto("macarrão", "parafuso", 3, 15);
 		assertTrue(estoque.getProdutos().size() == 2);
 	}
 	
 	@Test
 	public void buscarProdutoUmDeveRetornarProdutoUm() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-		estoque.cadastrarProduto("macarrão", "parafuso", 3, 15);
-		assertEquals(10, estoque.buscarProduto(10).getCodigo());
+		assertEquals(1, estoque.buscarProduto(1).getCodigo());
 	}
 	
 	@Test
 	public void buscarProdutoQueNaoExisteDeveRetornarRuntimeException() {
-		Estoque estoque = new Estoque();
 		assertThrows(RuntimeException.class, () -> {
-			estoque.buscarProduto(5).getCodigo();
+			estoque.buscarProduto(10).getCodigo();
 		});
 	}
 	
 	@Test
 	public void adicionarQuantidadeNegativaDeveRetornarIllegalArgumentException() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
 		assertThrows(IllegalArgumentException.class, () -> {
-			estoque.adicionarEstoque(11, -1);
+			estoque.adicionarEstoque(1, -1);
 		});
 	}
 	
 	@Test
 	public void adicionarDoisETresNoEstoqueDeveRetornarCinco() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-		estoque.adicionarEstoque(6, 2);
-		estoque.adicionarEstoque(6, 3);
-		assertEquals(5, estoque.getProdutos().get(estoque.buscarProduto(6)));
+		estoque.adicionarEstoque(1, 2);
+		estoque.adicionarEstoque(1, 3);
+		assertEquals(5, estoque.getProdutos().get(estoque.buscarProduto(1)));
 	}
 	
 	@Test
 	public void adicionarQuantidadeEmProdutoQueNaoTemNoEstoqueDeveRetornarRuntimeException() {
-		Estoque estoque = new Estoque();
 		assertThrows(RuntimeException.class, () -> {
-			estoque.adicionarEstoque(3, 3);
+			estoque.adicionarEstoque(10, 1);
 		});
 	}
 	
 	@Test
 	public void removerQuantidadeNegativaDeveRetornarIllegalArgumentException() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
 		assertThrows(IllegalArgumentException.class, () -> {
-			estoque.removerEstoque(8, -1);
+			estoque.removerEstoque(1, -1);
 		});
 	}
 	
 	@Test
 	public void removerDoisDeCincoNoEstoqueDeveRetornarTres() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-		estoque.adicionarEstoque(1, 5);
-		estoque.removerEstoque(1, 2);
-		assertEquals(3, estoque.getProdutos().get(estoque.buscarProduto(1)));
+		estoque.adicionarEstoque(2, 5);
+		estoque.removerEstoque(2, 2);
+		assertEquals(3, estoque.getProdutos().get(estoque.buscarProduto(2)));
 	}
 	
 	@Test
 	public void removerValorSuperiorAoEstoqueDeveRetornarRuntimeException() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-		estoque.adicionarEstoque(7, 3);
 		assertThrows(RuntimeException.class, () -> {
-			estoque.removerEstoque(7, 6);
+			estoque.removerEstoque(2, 10);
 		});
 	}
 	
 	@Test
 	public void removerQuantidadeEmProdutoQueNaoTemNoEstoqueDeveRetornarRuntimeException() {
-		Estoque estoque = new Estoque();
 		assertThrows(RuntimeException.class, () -> {
-			estoque.removerEstoque(3, 3);
+			estoque.removerEstoque(10, 2);
 		});
 	}
 	
 	@Test
 	public void listarProdutosCompleto() {
-		Estoque estoque = new Estoque();
-		estoque.cadastrarProduto("arroz", "branco", 5, 10);
-		estoque.cadastrarProduto("macarrão", "parafuso", 3, 15);
-		estoque.adicionarEstoque(3, 20);
-		estoque.adicionarEstoque(2, 30);
-		assertEquals("Código: 2 | Nome: arroz | Descrição: branco | Preço: 5.0 | Validade: 10 | Quantidade: 30\n"
-					+"Código: 3 | Nome: macarrão | Descrição: parafuso | Preço: 3.0 | Validade: 15 | Quantidade: 20", estoque.listarProdutos());
+		assertEquals("Código: 1 | Nome: arroz | Descrição: branco | Preço: 5.0 | Validade: 10 | Quantidade: 0\n"
+					+"Código: 2 | Nome: macarrão | Descrição: parafuso | Preço: 3.0 | Validade: 15 | Quantidade: 3", estoque.listarProdutos());
 	}
 
 }
